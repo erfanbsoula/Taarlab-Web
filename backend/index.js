64deb9c11951fcc819ec49694bb3e6ea8d11add2
erfan <90113405+imerfan-b@users.noreply.github.com>
2022-07-25 14:59:45 +0430

    added login page

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'frontend')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.all('*', (req, res) => {
+	res.status(404);
+	res.send('<h1>Error: 404 not found!<h1>');
 })
+
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
