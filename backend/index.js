b5f732fee8aff98e3fd66d7ed25c44428afbd16e
erfan <90113405+imerfan-b@users.noreply.github.com>
2022-08-02 15:47:36 +0430

    implemeneted authentication without passport

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const { client, db } = require('./mongoTest.js');
 const MongoStore = require('connect-mongo');
 const session = require("express-session");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local").Strategy;
 const http = require('http');
 const path = require('path');
 const app = express();
 const port = 3000;
 
 db.collection("sessions").drop((err, delOK) => {
-    if (err) {
+	if (err) {
 		console.log(err.errmsg);
 	};
-    if (delOK) {
+	if (delOK) {
 		console.log("sessions collection deleted");
 	}
 	let adminDB = db.admin();
+	console.log('listing the database:')
 	adminDB.listDatabases(function(err, result) {
 		if (err) {
 			console.log(err.errmsg);
@@ -22,6 +25,9 @@ db.collection("sessions").drop((err, delOK) => {
 		console.log(result.databases);
 	}); 
 });
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
 	secret: 'keyboard cat',
@@ -41,21 +47,117 @@ app.use(session({
 	}
 }));
 
-app.get('/', (req, res) => {
+// passport.use(new LocalStrategy(
+// 	{
+// 		usernameField: 'username',
+// 		passwordField: 'password'
+// 	},
+// 	(username, password, done) => {
+// 		db.collection("users").findOne({ username: username }, function (err, user) {
+// 			if (err) { return done(err); }
+// 			if (!user) { return done(null, false); }
+// 			if (user.password != password) { return done(null, false); }
+// 			return done(null, user);
+// 		});
+// 	}
+// ))
+
+// passport.serializeUser(function(user, done) {
+// 	done(null, user._id);
+// });
+
+// passport.deserializeUser(function(id, done) {
+// 	db.collection("users").findOne({ _id: id }, function (err, user) {
+// 		console.log(`deserialized user is ${user}`)
+// 		done(err, user);
+// 	});
+// });
+
+// app.use(passport.initialize());
+
+function printdb () {
 	db.collection("sessions").find({}).toArray(function(err, result) {
 		if (err) throw err;
 		console.log(result);
 	});
+	db.collection("users").find({}).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+	});
+}
+
+app.get('/', (req, res) => {
+	let text = "";
 	if (req.session.counter) {
 		req.session.counter++;
-		res.send(`<h3>you are the session #${req.session.counter}</h3>`);
-		return;
+		text = `<h3>you are the session #${req.session.counter}</h3>`;
 	}
-	req.session.counter = 1;
-	res.send(`<h3>counter set</h3>`);
+	else {
+		req.session.counter = 1;
+		text = `<h3>counter set</h3>`;
+	}
+	if (req.session.loggedIn) {
+		text += "<h4>logged in<h4>";
+	}
+	res.send(text);
 })
 
-// app.use(express.static(path.join(__dirname, '..', 'frontend')))
+app.get('/login', (req, res) => {
+	if (req.session.loggedIn) {
+		res.send('you are already logged in!');
+		return;
+	}
+	res.sendFile(path.join(__dirname, '..', 'frontend', 'login.html'));
+})
+
+app.get('/signup', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', 'frontend', 'signup.html'));
+})
+
+app.post('/signup', (req, res) => {
+	db.collection("users").insertOne({
+		username: req.body.username,
+		password: req.body.password
+	});
+	res.redirect('/login');
+})
+
+app.post('/login', (req, res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+	db.collection("users").findOne({ username: username }, function (err, user) {
+		if (err) {
+			console.log(err.message);
+			res.send('something went wrong...');
+			return;
+		}
+		if (!user) {
+			res.send('wrong username');
+			return;
+		}
+		if (user.password != password) {
+			res.send('wrong password');
+			return;
+		}
+		req.session.loggedIn = true;
+		res.redirect('/login-success')
+	});
+})
+
+app.get('/print', (req, res) => {
+	printdb();
+	res.send("done!")
+})
+
+app.get('/login-failure', (req, res) => {
+	res.send('login failed!')
+})
+
+app.get('/login-success', (req, res) => {
+	res.send('login successful!')
+})
+
+app.use(express.static(path.join(__dirname, '..', 'frontend')))
 
 // app.all('*', (req, res) => {
 // 	res.status(404);
@@ -65,6 +167,7 @@ app.get('/', (req, res) => {
 app.get('/close', (req, res) => {
 	client.close();
 	console.log('database connection closed!');
+	res.send('done!')
 })
 
 const server = http.createServer(app);
